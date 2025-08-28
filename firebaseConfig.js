9662c6b71a79333c88c1f03833a377a51e2027ec
ShekinaMaurin <maurinshekay@gmail.com>
2025-08-28 11:43:11 +0800

    firebase configuration

---
+++ firebaseConfig.js
@@ -0,0 +1,16 @@
+
+import { initializeApp } from "firebase/app";
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAsikJEm-hN0Je1a2LO6ySAr7GmEgnsn94",
+  authDomain: "smm-goal-tracker.firebaseapp.com",
+  projectId: "smm-goal-tracker",
+  storageBucket: "smm-goal-tracker.firebasestorage.app",
+  messagingSenderId: "676524423429",
+  appId: "1:676524423429:web:e6250af0b542a975d35f6c"
+};
+
+
+const app = initializeApp(firebaseConfig);
